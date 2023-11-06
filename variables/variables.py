a5a975e027ce259eb71986420875082a01e6236a
Gustavo <gustavosantossilveira12360@gmail.com>
2023-11-06 20:10:54 -0300

    variables

---
+++ variables/variables.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+name = "Gustavo"
+age = 20
+
+nameStr = str("Gustavo") 
+ageInt = int(20)
+
+print(type(nameStr))
+print(type(ageInt))
+
+print("My name is "+name+" and im "+str(age))
