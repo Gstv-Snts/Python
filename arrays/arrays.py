8559657ff6b8cf99d34c8d297c4c64334870d551
Gustavo <gustavosantossilveira12360@gmail.com>
2023-11-07 11:23:27 -0300

    arrays

---
+++ arrays/arrays.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+
+fruits = ["apple", "banana", "pear"]
+print(fruits)
+fruits.append("pineaple")
+print(fruits)
+print(len(fruits))
+fruits.pop(0)
+print(fruits)
+fruits.remove("pear")
+print(fruits)
