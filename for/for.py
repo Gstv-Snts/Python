8e81bab6e7ca528803c2db4bf40e6e3298aeeb78
Gustavo <gustavosantossilveira12360@gmail.com>
2023-11-07 11:23:44 -0300

    for loops

---
+++ for/for.py
@@ -0,0 +1,22 @@
+#!/bin/python3
+
+fruits = ["apple", "banana", "pear", "pinaple"]
+
+for fruit in fruits:
+    print(fruit)
+for letter in "banana":
+    print(letter)
+for number in "12345":
+    if number == "4":
+        break
+    print(number)
+for letter in "pineaxle":
+    if letter == "x":
+        continue
+    print(letter)
+for i in range(5):
+    print(i)
+for fruit in fruits:
+    print(fruit)
+else:
+    print("No more fruits")
